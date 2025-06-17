33af69c5f96dcea7491c83b84037e7274ad29a08
Adi <adisneh@gmail.com>
2025-06-17 20:31:33 +0300

    Convert to aiokafka in preparation for distributed consuming

--- src/kafka_key_breakdown/__main__.py
+++ src/kafka_key_breakdown/__main__.py
@@ -1,9 +1,10 @@
+import asyncio
 import logging
 import sys
 from collections import Counter
 from datetime import datetime, timedelta
 
-from confluent_kafka import Consumer, OFFSET_END, TopicPartition
+from aiokafka import AIOKafkaConsumer, TopicPartition
 
 from .graph import graph
 from .utils import consecutive_differences, range_datetime
@@ -16,7 +17,7 @@ handler.setFormatter(
 log.addHandler(handler)
 
 
-def get_key_distribution_for_timespan(
+async def get_key_distribution_for_timespan(
     topic: str,
     partition: int,
     bootstrap_servers: str,
@@ -26,70 +27,85 @@ def get_key_distribution_for_timespan(
     interval: timedelta,
     max_num_messages: int,
 ) -> dict[datetime, Counter[str]]:
-    consumer = Consumer(
-        **{
-            "bootstrap.servers": bootstrap_servers,
-            "group.id": consumer_group,
-        }
+    consumer = AIOKafkaConsumer(
+        topic,
+        bootstrap_servers=bootstrap_servers,
+        group_id=consumer_group,
     )
-    intervals = range_datetime(start_time, end_time, interval)
-    offsets_for_intervals = dict()
-    for t in intervals:
-        offset = consumer.offsets_for_times(
-            [TopicPartition(topic, partition, offset=int(t.timestamp() * 1000))],
-            timeout=1,
-        )[0].offset
-        # TODO: Also slice out the intervals that are earlier than the earliest record time
-        if offset == OFFSET_END:
-            log.warning(
-                f"Timestamp {t} is later than the latest record time "
-                f"for the topic {topic}, partition {partition}"
-            )
-        else:
-            offsets_for_intervals[t] = offset
-    if not offsets_for_intervals:
-        return dict()
+    await consumer.start()
+    # TODO: Make a context manager or some other nicer way to close the consumer
+    try:
+        intervals = range_datetime(start_time, end_time, interval)
+        offsets_for_intervals = dict()
+        topic_partition = TopicPartition(topic, partition)
+        beginning_offset = (await consumer.beginning_offsets([topic_partition]))[topic_partition]
+        end_offset = (await consumer.end_offsets([topic_partition]))[topic_partition]
+        for t in intervals:
+            offset_and_timestamp = (await consumer.offsets_for_times(
+                {topic_partition: int(t.timestamp() * 1000)}
+            ))[topic_partition]
+            if offset_and_timestamp is None:
+                log.warning(
+                    f"Timestamp {t} is later than the latest record time "
+                    f"for the topic {topic}, partition {partition}. Skipping."
+                )
+            elif offset_and_timestamp.offset == beginning_offset:
+                log.warning(
+                    f"Timestamp {t} yielded the same offset as the first record in the "
+                    f"topic {topic}, partition {partition}, meaning that the timestamp is probable earlier "
+                    f"than the earliest record time. Skipping."
+                )
+            else:
+                offsets_for_intervals[t] = offset_and_timestamp.offset
+        if not offsets_for_intervals:
+            return dict()
 
-    earliest_offset, latest_offset = consumer.get_watermark_offsets(TopicPartition(topic, partition))
-    offsets = list(offsets_for_intervals.values())
-    num_messages_for_intervals = [
-        min(d, max_num_messages)
-        for d in consecutive_differences(offsets)
-    ] + [min(latest_offset - offsets[-1], max_num_messages)]
-    return {
-        t: get_key_distribution_by_offset(
-            topic=topic,
-            partition=partition,
-            consumer=consumer,
-            offset=max(offset - num_messages_for_intervals[i], earliest_offset),
-            num_messages=num_messages_for_intervals[i],
-        )
-        for i, (t, offset) in enumerate(offsets_for_intervals.items())
-    }
+        offsets = list(offsets_for_intervals.values())
+        num_messages_for_intervals = [
+            min(d, max_num_messages)
+            for d in consecutive_differences(offsets)
+        ] + [min(end_offset - offsets[-1], max_num_messages)]
+        key_distribution = {
+            t: (await get_key_distribution_by_offset(
+                topic_partition=topic_partition,
+                consumer=consumer,
+                offset=max(offset - num_messages_for_intervals[i], beginning_offset),
+                num_messages=num_messages_for_intervals[i],
+            )) if num_messages_for_intervals[i] > 0 else Counter()
+            for i, (t, offset) in enumerate(offsets_for_intervals.items())
+        }
+    finally:
+        await consumer.stop()
+    return key_distribution
 
 
-def get_key_distribution_by_offset(
-    topic: str,
-    partition: int,
-    consumer: Consumer,
+async def get_key_distribution_by_offset(
+    topic_partition: TopicPartition,
+    consumer: AIOKafkaConsumer,
     offset: int,
     num_messages: int,
 ) -> Counter[str] | None:
-    consumer.assign([TopicPartition(topic, partition, offset=offset)])
-    messages = consumer.consume(num_messages=num_messages, timeout=1)
-    return Counter(m.key().decode() for m in messages)
+    consumer.seek(topic_partition, offset)
+    messages = (
+        await consumer.getmany(
+            topic_partition,  # type: ignore
+            timeout_ms=1000,
+            max_records=num_messages,
+        )
+    )[topic_partition]
+    return Counter(m.key.decode() for m in messages)
 
 
 if __name__ == '__main__':
     graph(
-        get_key_distribution_for_timespan(
+        asyncio.run(get_key_distribution_for_timespan(
             topic="adi",
             partition=0,
             bootstrap_servers="localhost:9092",
             consumer_group="adi",
-            start_time=datetime(2025, 6, 17, 15, 0, 34),
-            end_time=datetime(2025, 6, 17, 15, 17, 34),
+            start_time=datetime(2025, 6, 17, 19, 39, 0),
+            end_time=datetime(2025, 6, 17, 19, 49, 40),
             interval=timedelta(minutes=1),
             max_num_messages=50,
-        )
+        ))
     )
