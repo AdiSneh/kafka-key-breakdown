d72dc1ca1904f2f5954b1e0935c5fb8238c42945
Adi <adisneh@gmail.com>
2025-06-18 00:12:40 +0300

    Clean up code a bit

--- src/kafka_key_breakdown/__main__.py
+++ src/kafka_key_breakdown/__main__.py
@@ -7,7 +7,7 @@ from datetime import datetime, timedelta
 from aiokafka import AIOKafkaConsumer, TopicPartition
 
 from .graph import graph
-from .utils import consecutive_differences, gather_with_limit, range_datetime
+from .utils import gather_with_limit, range_datetime
 
 log = logging.Logger("Kafka Key Breakdown")
 handler = logging.StreamHandler(sys.stdout)
@@ -28,60 +28,72 @@ async def get_key_distribution_for_timespan(
     max_num_messages: int,
     concurrency_limit: int,
 ) -> dict[datetime, Counter[str]]:
-    intervals = range_datetime(start_time, end_time, interval)
-    offsets_for_intervals = dict()
     topic_partition = TopicPartition(topic, partition)
-
     async with AIOKafkaConsumer(bootstrap_servers=bootstrap_servers, group_id=consumer_group) as consumer:
-        beginning_offset = (await consumer.beginning_offsets([topic_partition]))[topic_partition]
-        end_offset = (await consumer.end_offsets([topic_partition]))[topic_partition]
-        for t in intervals:
-            offset_and_timestamp = (await consumer.offsets_for_times(
-                {topic_partition: int(t.timestamp() * 1000)}
-            ))[topic_partition]
-            if offset_and_timestamp is None:
-                log.warning(
-                    f"Timestamp {t} is later than the latest record time "
-                    f"for the topic {topic}, partition {partition}. Skipping."
-                )
-            elif offset_and_timestamp.offset == beginning_offset:
-                log.warning(
-                    f"Timestamp {t} yielded the same offset as the first record in the "
-                    f"topic {topic}, partition {partition}, meaning that the timestamp is probable earlier "
-                    f"than the earliest record time. Skipping."
-                )
-            else:
-                offsets_for_intervals[t] = offset_and_timestamp.offset
-
-    if not offsets_for_intervals:
+        first_offset = (await consumer.beginning_offsets([topic_partition]))[topic_partition]
+        last_offset = (await consumer.end_offsets([topic_partition]))[topic_partition]
+        intervals_to_offsets = await _get_intervals_to_offsets(
+            consumer=consumer, topic_partition=topic_partition, start_time=start_time, end_time=end_time,
+            interval=interval, first_offset=first_offset
+            )
+    if not intervals_to_offsets:
         return dict()
 
-    offsets = list(offsets_for_intervals.values())
-    num_messages_for_intervals = [
-        min(d, max_num_messages)
-        for d in consecutive_differences(offsets)
-    ] + [min(end_offset - offsets[-1], max_num_messages)]
-    consume_coroutines = [
-        get_key_distribution_by_offset(
-            topic_partition=topic_partition,
-            bootstrap_servers=bootstrap_servers,
-            consumer_group=f"{consumer_group}-{i % concurrency_limit}",
-            offset=max(offset - num_messages_for_intervals[i], beginning_offset),
-            num_messages=num_messages_for_intervals[i],
+    consume_coroutines = []
+    for i, offset in enumerate(offsets := list(intervals_to_offsets.values())):
+        num_messages = min(
+            (
+                offsets[i + 1] - offset
+                if i < len(offsets) - 1
+                else last_offset - offsets[-1]
+            ),
+            max_num_messages,
         )
-        for i, offset in enumerate(offsets_for_intervals.values())
-    ]
+        consume_coroutines.append(
+            _get_key_distribution_by_offset(
+                topic_partition=topic_partition, bootstrap_servers=bootstrap_servers,
+                consumer_group=f"{consumer_group}-{i % concurrency_limit}",
+                offset=max(offset - num_messages, first_offset), num_messages=num_messages
+                )
+        )
     results = await gather_with_limit(*consume_coroutines, limit=concurrency_limit)
-    return dict(zip(intervals, results))
+    return dict(zip(intervals_to_offsets.keys(), results))
 
 
-async def get_key_distribution_by_offset(
+async def _get_intervals_to_offsets(
+    consumer: AIOKafkaConsumer,
+    topic_partition: TopicPartition,
+    start_time: datetime,
+    end_time: datetime,
+    interval: timedelta,
+    first_offset: int,
+) -> dict[datetime, int]:
+    intervals_to_offsets = dict()
+    for t in range_datetime(start_time, end_time, interval):
+        offset_and_timestamp = (await consumer.offsets_for_times(
+            {topic_partition: int(t.timestamp() * 1000)}
+        ))[topic_partition]
+        if offset_and_timestamp is None:
+            log.warning(
+                f"Timestamp {t} is later than the latest record time in {topic_partition}. Skipping."
+            )
+        elif offset_and_timestamp.offset == first_offset:
+            log.warning(
+                f"Timestamp {t} yielded the same offset as the first record in topic partition {topic_partition}, "
+                f"meaning that the timestamp is probably earlier than the earliest record time. Skipping."
+            )
+        else:
+            intervals_to_offsets[t] = offset_and_timestamp.offset
+    return intervals_to_offsets
+
+
+async def _get_key_distribution_by_offset(
     topic_partition: TopicPartition,
     bootstrap_servers: str,
     consumer_group: str,
     offset: int,
     num_messages: int,
-) -> Counter[str] | None:
+) -> Counter[str]:
     if num_messages == 0:
         return Counter()
     async with AIOKafkaConsumer(bootstrap_servers=bootstrap_servers, group_id=consumer_group) as consumer:
